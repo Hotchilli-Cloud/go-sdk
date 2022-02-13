fb6ec16f983ef13bf80b5021735de0e7186716bd
George Turner <49865096+george-turner@users.noreply.github.com>
2022-02-13 13:09:22 +0000

    initial commit

--- cmd/hcloud/numbers.go
+++ cmd/hcloud/numbers.go
@@ -7,7 +7,7 @@ import (
 
 //ctx context.Context, options *GetNumberRequest
 func (c *Client) GetSummary() (*SuccessResponse, error) {
-	req, err := http.NewRequest("GET", fmt.Sprintf("&s/node-api/numbers/data/summary?client_id=%d&year=%d", c.BaseURL, 2645, 2021), nil)
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/node-api/numbers/data/summary?client_id=%d&year=%d", c.BaseURL, "2645", "2021"), nil)
 	if err != nil {
 		return nil, err
 	}
